dc9e9996665e8af78fc406750b88c284f9cabee6
Mark Andreev <m.andreev@conundrum.ai>
2019-03-15 19:33:14 +0300

    Add TornadoHandler with implementation of all modules

--- tornado_swagger/handlers.py
+++ tornado_swagger/handlers.py
@@ -1,4 +1,12 @@
+from typing import Awaitable
+from typing import Optional
+
 import tornado.web
+
+
+class TornadoHandler(tornado.web.RequestHandler):
+    def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
+        pass
 
 
 class SwaggerHomeHandler(tornado.web.RequestHandler):
