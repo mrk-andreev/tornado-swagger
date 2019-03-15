075d6ce1401bea89c96dd454444eb111b8fb90ab
Mark Andreev <m.andreev@conundrum.ai>
2019-03-15 19:38:19 +0300

    Remove Awaitable typing from handlers

--- tornado_swagger/handlers.py
+++ tornado_swagger/handlers.py
@@ -1,11 +1,8 @@
-from typing import Awaitable
-from typing import Optional
-
 import tornado.web
 
 
 class TornadoHandler(tornado.web.RequestHandler):
-    def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
+    def data_received(self, chunk: bytes):
         pass
 
 
