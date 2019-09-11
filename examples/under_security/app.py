54c1003341f7203fc0a763a0a199ca02f77596a1
Mark Andreev <m.andreev@conundrum.ai>
2019-09-11 13:54:34 +0300

    Add example auth for swagger
    https://github.com/mrk-andreev/tornado-swagger/issues/11

---
+++ examples/under_security/app.py
@@ -0,0 +1,65 @@
+import tornado.web
+
+
+class ExampleHandler(tornado.web.RequestHandler):
+    def get(self):
+        """
+        Description end-point
+
+        ---
+        tags:
+        - Example
+        summary: Create user
+        description: This can only be done by the logged in user.
+        operationId: examples.api.api.createUser
+        produces:
+        - application/json
+        parameters:
+        - in: body
+          name: body
+          description: Created user object
+          required: false
+          schema:
+            type: object
+            properties:
+              id:
+                type: integer
+                format: int64
+              username:
+                type:
+                  - "string"
+                  - "null"
+              firstName:
+                type: string
+              lastName:
+                type: string
+              email:
+                type: string
+              password:
+                type: string
+              phone:
+                type: string
+              userStatus:
+                type: integer
+                format: int32
+                description: User Status
+        responses:
+        "201":
+          description: successful operation
+        """
+        self.write({})
+
+
+class Application(tornado.web.Application):
+    _routes = [
+        tornado.web.url(r'/api/example', ExampleHandler, name='example'),
+    ]
+
+    def __init__(self):
+        settings = {
+            'debug': True
+        }
+        from tornado_swagger.setup import setup_swagger
+
+        setup_swagger(self._routes)
+        super(Application, self).__init__(self._routes, **settings)
---
+++ examples/under_security/under_security.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado_http_auth import BasicAuthMixin
+
+from tornado_swagger import _handlers
+from app import Application
+
+credentials = {
+    'user1': 'pass1'
+}
+
+
+class SwaggerHomeHandlerSecure(BasicAuthMixin, _handlers.SwaggerHomeHandler):
+    def prepare(self):
+        self.get_authenticated_user(check_credentials_func=credentials.get, realm='Protected')
+
+
+_handlers.SwaggerHomeHandler = SwaggerHomeHandlerSecure
+
+if __name__ == '__main__':
+    tornado.options.define('port', default='8080', help='Port to listen on')
+    tornado.options.parse_command_line()
+
+    app = Application()
+    app.listen(port=8081)
+
+    tornado.ioloop.IOLoop.current().start()
