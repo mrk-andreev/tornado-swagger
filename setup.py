af3df5a627a1bd6f9c671034b681f7d3f1c300e0
Mark Andreev <mark.andreev@gmail.com>
2022-08-04 12:16:26 +0400

    Update dependencies (#60)
    - Update dev requirements (fix broken packages)
    - Update PyYAML from `PyYAML==5.4` to `PyYAML>=5.4` [issue-59](https://github.com/mrk-andreev/tornado-swagger/issues/59)
    - Specify encoding in `tornado_swagger/setup.py::open`

--- setup.py
+++ setup.py
@@ -22,6 +22,10 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
