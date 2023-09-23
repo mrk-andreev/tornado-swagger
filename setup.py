e472fa1998a38b79611b793e4305d1240a5421cc
Mark Andreev <mark.andreev@gmail.com>
2023-09-23 20:33:38 +0100

    Add release notes

--- setup.py
+++ setup.py
@@ -19,8 +19,6 @@ CLASSIFIERS = [
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
