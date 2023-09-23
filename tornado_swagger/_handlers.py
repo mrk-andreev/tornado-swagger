e7e8e11a167655f75dbfe768d4d16a7deccd9ecf
Mark Parker <markitstep@gmail.com>
2023-09-23 15:47:49 +0300

    add allow_cors option (#73)

--- tornado_swagger/_handlers.py
+++ tornado_swagger/_handlers.py
@@ -8,13 +8,27 @@ class TornadoBaseHandler(tornado.web.RequestHandler):
 
 class SwaggerUiHandler(TornadoBaseHandler):
     SWAGGER_HOME_TEMPLATE = ""
+    allow_cors: bool = False
 
     def get(self):
         self.write(self.SWAGGER_HOME_TEMPLATE)
+        
+    def options(self):
+        if self.allow_cors:
+            self.set_header("Access-Control-Allow-Origin", "*")
+            self.set_header("Access-Control-Allow-Headers", "Content-Type")
+            self.set_header("Access-Control-Allow-Methods", "GET, OPTIONS")
 
 
 class SwaggerSpecHandler(TornadoBaseHandler):
     SWAGGER_SPEC = ""
+    allow_cors: bool = False
 
     def get(self):
         self.write(self.SWAGGER_SPEC)
+        
+    def options(self):
+        if self.allow_cors:
+            self.set_header("Access-Control-Allow-Origin", "*")
+            self.set_header("Access-Control-Allow-Headers", "Content-Type")
+            self.set_header("Access-Control-Allow-Methods", "GET, OPTIONS")
